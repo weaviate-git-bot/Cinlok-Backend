50a879f716c2ac7a4e973a99d53d2f16de2d7620
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2023-02-19 02:17:46 +0700

    feat: add auth middleware and self endpoint

---
+++ src/middleware/auth/admin-authenticated.ts
@@ -0,0 +1,26 @@
+import { Role } from "@prisma/client";
+import { UnauthenticatedError } from "../../error";
+import jwt from "../../lib/jwt";
+import prisma from "../../repository";
+import { AsyncMiddleware } from "../async-wrapper";
+
+export const AuthMiddleware = AsyncMiddleware(async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    throw new UnauthenticatedError();
+  }
+  const { id } = jwt.verify(token) as { id: number };
+  const user = await prisma.account.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!user || user.role !== Role.ADMIN) {
+    throw new UnauthenticatedError();
+  }
+
+  res.locals['account'] = user
+
+  next();
+});
--- src/route/auth.ts
+++ src/route/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { authController } from "../controller";
+import { AuthMiddleware } from "../middleware/auth/user-authenticated";
 
 const AuthRoute = Router()
 AuthRoute.post("/login", authController.login)
 AuthRoute.post("/register", authController.register)
+AuthRoute.get("/self", AuthMiddleware, authController.self)
 
 export default AuthRoute
